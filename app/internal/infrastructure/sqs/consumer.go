6b8a2cbec51438bee7fdb3edc8e2d4c470ecca1d
william-cesar-santos1 <william.cesar.santos1@gmail.com>
2026-07-04 23:01:08 -0300

    fix indentation

--- app/internal/infrastructure/sqs/consumer.go
+++ app/internal/infrastructure/sqs/consumer.go
@@ -1,102 +1,102 @@
 package sqs
 
 import (
-"context"
-"encoding/json"
+	"context"
+	"encoding/json"
 
-appusecase "github.com/WilliamCesarSantos/movie-suggestion-api/app/internal/application/usecase"
-"github.com/WilliamCesarSantos/movie-suggestion-api/app/internal/infrastructure/http/middleware"
-awssqs "github.com/aws/aws-sdk-go-v2/service/sqs"
-"github.com/aws/aws-sdk-go-v2/service/sqs/types"
-"github.com/google/uuid"
-"github.com/rs/zerolog"
+	appusecase "github.com/WilliamCesarSantos/movie-suggestion-api/app/internal/application/usecase"
+	"github.com/WilliamCesarSantos/movie-suggestion-api/app/internal/infrastructure/http/middleware"
+	awssqs "github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"github.com/google/uuid"
+	"github.com/rs/zerolog"
 )
 
 type Consumer struct {
-sqsClient   *awssqs.Client
-queueURL    string
-workerCount int
-processor   appusecase.ProcessMovieImportUseCase
-logger      zerolog.Logger
+	sqsClient   *awssqs.Client
+	queueURL    string
+	workerCount int
+	processor   appusecase.ProcessMovieImportUseCase
+	logger      zerolog.Logger
 }
 
 func NewConsumer(sqsClient *awssqs.Client, queueURL string, workerCount int, processor appusecase.ProcessMovieImportUseCase, logger zerolog.Logger) *Consumer {
-return &Consumer{
-sqsClient:   sqsClient,
-queueURL:    queueURL,
-workerCount: workerCount,
-processor:   processor,
-logger:      logger,
-}
+	return &Consumer{
+		sqsClient:   sqsClient,
+		queueURL:    queueURL,
+		workerCount: workerCount,
+		processor:   processor,
+		logger:      logger,
+	}
 }
 
 func (c *Consumer) Start(ctx context.Context) {
-msgCh := make(chan types.Message, c.workerCount)
+	msgCh := make(chan types.Message, c.workerCount)
 
-for i := 0; i < c.workerCount; i++ {
-go c.worker(ctx, msgCh)
-}
+	for i := 0; i < c.workerCount; i++ {
+		go c.worker(ctx, msgCh)
+	}
 
-for {
-select {
-case <-ctx.Done():
-close(msgCh)
-return
-default:
-out, err := c.sqsClient.ReceiveMessage(ctx, &awssqs.ReceiveMessageInput{
-QueueUrl:              &c.queueURL,
-MaxNumberOfMessages:   10,
-WaitTimeSeconds:       20,
-MessageAttributeNames: []string{"correlationId"},
-})
-if err != nil {
-c.logger.Error().Str("correlationId", "system").Err(err).Msg("SQS receive error")
-continue
-}
-for _, msg := range out.Messages {
-select {
-case <-ctx.Done():
-return
-case msgCh <- msg:
-}
-}
-}
-}
+	for {
+		select {
+		case <-ctx.Done():
+			close(msgCh)
+			return
+		default:
+			out, err := c.sqsClient.ReceiveMessage(ctx, &awssqs.ReceiveMessageInput{
+				QueueUrl:              &c.queueURL,
+				MaxNumberOfMessages:   10,
+				WaitTimeSeconds:       20,
+				MessageAttributeNames: []string{"correlationId"},
+			})
+			if err != nil {
+				c.logger.Error().Str("correlationId", "system").Err(err).Msg("SQS receive error")
+				continue
+			}
+			for _, msg := range out.Messages {
+				select {
+				case <-ctx.Done():
+					return
+				case msgCh <- msg:
+				}
+			}
+		}
+	}
 }
 
 func (c *Consumer) worker(ctx context.Context, msgCh <-chan types.Message) {
-for msg := range msgCh {
-messageID := ""
-if msg.MessageId != nil {
-messageID = *msg.MessageId
-}
+	for msg := range msgCh {
+		messageID := ""
+		if msg.MessageId != nil {
+			messageID = *msg.MessageId
+		}
 
-correlationID := "system"
-if attr, ok := msg.MessageAttributes["correlationId"]; ok && attr.StringValue != nil {
-correlationID = *attr.StringValue
-} else {
-correlationID = uuid.New().String()
-}
+		correlationID := "system"
+		if attr, ok := msg.MessageAttributes["correlationId"]; ok && attr.StringValue != nil {
+			correlationID = *attr.StringValue
+		} else {
+			correlationID = uuid.New().String()
+		}
 
-msgCtx := context.WithValue(ctx, middleware.ContextKeyCorrelationID, correlationID)
-msgLogger := c.logger.With().Str("correlationId", correlationID).Str("messageId", messageID).Logger()
-msgCtx = msgLogger.WithContext(msgCtx)
+		msgCtx := context.WithValue(ctx, middleware.ContextKeyCorrelationID, correlationID)
+		msgLogger := c.logger.With().Str("correlationId", correlationID).Str("messageId", messageID).Logger()
+		msgCtx = msgLogger.WithContext(msgCtx)
 
-var im ImportMessage
-if err := json.Unmarshal([]byte(*msg.Body), &im); err != nil {
-msgLogger.Error().Err(err).Msg("failed to unmarshal SQS message")
-continue
-}
-if err := c.processor.Process(msgCtx, im.ImdbID); err != nil {
-msgLogger.Error().Err(err).Str("imdbId", im.ImdbID).Msg("failed to process movie import")
-continue
-}
-_, err := c.sqsClient.DeleteMessage(ctx, &awssqs.DeleteMessageInput{
-QueueUrl:      &c.queueURL,
-ReceiptHandle: msg.ReceiptHandle,
-})
-if err != nil {
-msgLogger.Error().Err(err).Msg("failed to delete SQS message")
-}
-}
+		var im ImportMessage
+		if err := json.Unmarshal([]byte(*msg.Body), &im); err != nil {
+			msgLogger.Error().Err(err).Msg("failed to unmarshal SQS message")
+			continue
+		}
+		if err := c.processor.Process(msgCtx, im.ImdbID); err != nil {
+			msgLogger.Error().Err(err).Str("imdbId", im.ImdbID).Msg("failed to process movie import")
+			continue
+		}
+		_, err := c.sqsClient.DeleteMessage(ctx, &awssqs.DeleteMessageInput{
+			QueueUrl:      &c.queueURL,
+			ReceiptHandle: msg.ReceiptHandle,
+		})
+		if err != nil {
+			msgLogger.Error().Err(err).Msg("failed to delete SQS message")
+		}
+	}
 }
--- app/internal/infrastructure/sqs/publisher.go
+++ app/internal/infrastructure/sqs/publisher.go
@@ -1,44 +1,44 @@
 package sqs
 
 import (
-"context"
-"encoding/json"
+	"context"
+	"encoding/json"
 
-"github.com/WilliamCesarSantos/movie-suggestion-api/app/internal/infrastructure/http/middleware"
-awssqs "github.com/aws/aws-sdk-go-v2/service/sqs"
-"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"github.com/WilliamCesarSantos/movie-suggestion-api/app/internal/infrastructure/http/middleware"
+	awssqs "github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 )
 
 type Publisher struct {
-client   *awssqs.Client
-queueURL string
+	client   *awssqs.Client
+	queueURL string
 }
 
 func NewPublisher(client *awssqs.Client, queueURL string) *Publisher {
-return &Publisher{client: client, queueURL: queueURL}
+	return &Publisher{client: client, queueURL: queueURL}
 }
 
 func (p *Publisher) Publish(ctx context.Context, imdbID string) error {
-msg := ImportMessage{ImdbID: imdbID}
-data, err := json.Marshal(msg)
-if err != nil {
-return err
-}
-body := string(data)
+	msg := ImportMessage{ImdbID: imdbID}
+	data, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	body := string(data)
 
-attrs := map[string]types.MessageAttributeValue{}
-if cid, ok := ctx.Value(middleware.ContextKeyCorrelationID).(string); ok && cid != "" {
-dataType := "String"
-attrs["correlationId"] = types.MessageAttributeValue{
-DataType:    &dataType,
-StringValue: &cid,
-}
-}
+	attrs := map[string]types.MessageAttributeValue{}
+	if cid, ok := ctx.Value(middleware.ContextKeyCorrelationID).(string); ok && cid != "" {
+		dataType := "String"
+		attrs["correlationId"] = types.MessageAttributeValue{
+			DataType:    &dataType,
+			StringValue: &cid,
+		}
+	}
 
-_, err = p.client.SendMessage(ctx, &awssqs.SendMessageInput{
-QueueUrl:          &p.queueURL,
-MessageBody:       &body,
-MessageAttributes: attrs,
-})
-return err
+	_, err = p.client.SendMessage(ctx, &awssqs.SendMessageInput{
+		QueueUrl:          &p.queueURL,
+		MessageBody:       &body,
+		MessageAttributes: attrs,
+	})
+	return err
 }
